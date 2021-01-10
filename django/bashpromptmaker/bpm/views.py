334b24d8e12da7b753dd1a5bde173cb51226e7c0
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-01-10 13:32:19 -0700

    user can create dummy variables

--- django/bashpromptmaker/bpm/views.py
+++ django/bashpromptmaker/bpm/views.py
@@ -7,6 +7,7 @@ from .serializers import BashPromptSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 class BashPromptList(APIView):
     """
@@ -19,3 +20,13 @@ class BashPromptList(APIView):
         bashPrompts = BashPrompt.objects.all()
         serializer = BashPromptSerializer(bashPrompts, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        """
+        Create a prompt
+        """
+        serializer = BashPromptSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
---
+++ notes.md
@@ -0,0 +1,5 @@
+https://dragonprogrammer.com/adding-detail-views-django-drf-api/
+
+docker-compose down && docker system prune -a
+
+docker exec -it dj bash
