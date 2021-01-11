2ef82d35cf88b51490d5642397e6ec7df0d0bc42
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-01-11 14:42:50 -0700

    added value line

--- src/app/mock-elements.ts
+++ src/app/mock-elements.ts
@@ -1,8 +1,8 @@
 import { Element } from './element';
 
 export const ELEMENTS: Element[] = [
-  { id: 11, name: 'working-dir', value: '!bash', color: "white" },
-  { id: 12, name: 'hostname', value: '!bash', color: "white" },
-  { id: 13, name: 'date', value: '!bash', color: "white" },
-  { id: 14, name: 'prompt', value: '!bash', color: "white" },
+  { id: 11, name: 'working-dir', value: '!pwd', color: "white" },
+  { id: 12, name: 'hostname', value: '!who', color: "white" },
+  { id: 13, name: 'date', value: '!date', color: "white" },
+  { id: 14, name: 'prompt', value: '!$', color: "white" },
 ];
