2abe2b74350f3bfde924f7853251cb288bc2b254
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-01-11 15:27:44 -0700

    adding actual code for values

--- src/app/mock-elements.ts
+++ src/app/mock-elements.ts
@@ -1,8 +1,12 @@
 import { Element } from './element';
 
+let start: string = '\\' + '[\\' + 'e[';
+let color: string = "37";
+let end: string = 'm\\' + ']'
+//\[\e["$PURPLE"m\][\d]
 export const ELEMENTS: Element[] = [
-  { id: 11, name: 'working-dir', value: '!pwd', color: "white" },
-  { id: 12, name: 'hostname', value: '!who', color: "white" },
-  { id: 13, name: 'date', value: '!date', color: "white" },
-  { id: 14, name: 'prompt', value: '!$', color: "white" },
+  { id: 11, name: 'working-dir', value: start + color + end + '\\w', color: "white" },
+  { id: 12, name: 'hostname', value: start + color + end + '\\h', color: "white" },
+  { id: 13, name: 'date', value: start + color + end + '\\d', color: "white" },
+  { id: 14, name: 'prompt', value: ':)', color: "white" },
 ];
