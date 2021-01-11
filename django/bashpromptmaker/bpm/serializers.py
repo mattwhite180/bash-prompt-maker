138ccbf8d029fc1e24bb8de13ea8da39b32c762e
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-01-10 17:00:25 -0700

    removed docker and django

--- django/bashpromptmaker/bpm/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import BashPrompt
-
-class BashPromptSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BashPrompt
-        fields = ('id', 'name', 'value', 'created_on')
