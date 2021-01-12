b7a5349c4a0a0a5167b06a43aba28643ab99fec1
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2021-01-11 18:57:58 -0700

    added what prompt may look like

--- src/app/mock-elements.ts
+++ src/app/mock-elements.ts
@@ -5,10 +5,10 @@ let color: string = "37";
 let end: string = 'm\\' + ']'
 //\[\e["$PURPLE"m\][\d]
 export const ELEMENTS: Element[] = [
-  { id: 11, name: 'working-dir', value: start + color + end + '\\w', color: "white" },
-  { id: 12, name: 'hostname', value: start + color + end + '\\h', color: "white" },
-  { id: 13, name: 'date', value: start + color + end + '\\d', color: "white" },
-  { id: 14, name: 'prompt', value: ':)', color: "white" },
-  { id: 14, name: 'username', value: 'usr', color: "white" },
-  { id: 14, name: '@', value: '@', color: "white" },
+  { id: 11, name: 'working-dir', value: start + color + end + '\\w', color: "white", view: "~/" },
+  { id: 12, name: 'hostname', value: start + color + end + '\\h', color: "white", view: "ubuntu" },
+  { id: 13, name: 'date', value: start + color + end + '\\d', color: "white", view: "OCT 31" },
+  { id: 14, name: 'prompt', value: ':)', color: "white", view: "[$]" },
+  { id: 14, name: 'username', value: 'usr', color: "white", view: "user" },
+  { id: 14, name: '@', value: '@', color: "white", view: "@" },
 ];
